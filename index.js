784f71097cb7261e05efa034c0bd87dbd3fba598
Anatolii Liutenko <anatoliianatolich@gmail.com>
2018-11-10 19:33:00 +0200

    delete db link

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const server = require("./server/server");
 const connectDB = require("./server/connectDB/conn");
 
 const port = 5050;
-const db = "mongodb://Anatolich:test987654321@ds143893.mlab.com:43893/registration";
 
 server(port);
 connectDB(db);
