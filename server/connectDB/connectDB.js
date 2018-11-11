1bbfa88204a22832a045157ce41ea05441a8417e
anatolianatolich <anatoliianatolich@gmail.com>
2018-11-11 20:44:40 +0200

    fix config

--- server/connectDB/connectDB.js
+++ server/connectDB/connectDB.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+const {DB} = require('../config/config');
 
 const connect = () => {
-    mongoose.connect('mongodb://Anatolich:test987654321@ds143893.mlab.com:43893/registration', { useNewUrlParser: true });
+    mongoose.connect(`${DB}`, { useNewUrlParser: true });
     const db = mongoose.connection;
     db.on('error', ()=> {
         console.log('error connect db');
     });
     db.once("open", ()=> {
         console.log("connect db");
-    })
+    });
 }
 
 module.exports = connect;
