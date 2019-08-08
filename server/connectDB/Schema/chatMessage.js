4a998ada1124e3eea707244995b3fc62d690ce51
Лютенко Анатолій Анатолійович <liutenko@ua.mti>
2019-08-08 16:49:14 +0300

    'models'

--- server/connectDB/Schema/chatMessage.js
+++ server/connectDB/Schema/chatMessage.js
@@ -3,6 +3,11 @@ const {Schema} = mongoose;
 
 const ChatMessageSchema = new Schema({
     id_chat: String,
+    data_chat: {
+        name: String,
+        date: Date,
+        message: String,
+    }
 })
 
 module.exports = mongoose.model('chatMessage', ChatMessageSchema, 'task');
